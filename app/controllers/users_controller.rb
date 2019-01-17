e6d684907231866972e66067a9a57486e0a4fd68
yoshidakai1998 <bkbkai130@gmail.com>
2019-01-17 16:22:21 +0900

    Mentor pointed out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
-class UsersController < ApplicationController
+class  UsersController < ApplicationController
 
-	def edit
+	def  edit
 	end
 
-	def update
-		if current_user.update(user_params)
+	def  update
+		if  current_user.update(user_params)
 			redirect_to root_path
 		else
-			render :edit
+		    render :edit
 		end
 	end
 
 	private
 
-	def user_params
-		params.require(:user).permit(:name, :email)
+	def  user_params
+		 params.require(:user).permit(:name, :email)
 	end
 
 end
