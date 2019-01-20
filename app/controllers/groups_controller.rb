b4f6affd46606a9ccc8b293400f2c659a0da48b8
yoshidakai1998 <bkbkai130@gmail.com>
2019-01-20 16:16:40 +0900

    Edit groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,6 +6,12 @@ class GroupsController < ApplicationController
   end
 
   def create
+  	@group = Group.new(group_params)
+  	if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
