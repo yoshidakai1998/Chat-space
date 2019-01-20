9e24ca96039444b60e0bacdcb2f14fc227c87bd0
yoshidakai1998 <bkbkai130@gmail.com>
2019-01-20 20:52:45 +0900

    Fixed points pointed out

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,8 +8,8 @@ class GroupsController < ApplicationController
   end
 
   def create
-  	@group = Group.new(group_params)
-  	if @group.save
+    @group = Group.new(group_params)
+    if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
@@ -23,8 +23,8 @@ class GroupsController < ApplicationController
   end
 
   private
-  def group_params
-  	params.require(:group).permit(:name, { :user_ids => []})
-  end
+    def group_params
+      params.require(:group).permit(:name, { :user_ids => []})
+    end
 
 end
