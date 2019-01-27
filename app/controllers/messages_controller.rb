72d6744929fa6b4167fd1cbb9768e8da3affbeac
yoshidakai1998 <bkbkai130@gmail.com>
2019-01-27 18:30:31 +0900

    Delete blank line ＆　Fix indentation
    空行の削除とインデントの修正をしました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,19 +2,19 @@ class MessagesController < ApplicationController
   before_action :set_group
 
 	def index
-      @message =Message.new
-      @messages = @group.messages.includes(:user)
+    @message =Message.new
+    @messages = @group.messages.includes(:user)
 	end
 
 	def create
-      @message = @group.messages.new(message_params)
-      if @message.save
-      	redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-      else
-      	@messages = @group.messages.includes(:user)
-      	flash.now[:alert] = 'メッセージを入力してください。'
-      	render :index
-      end
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
 	end
 
 	private
@@ -24,7 +24,7 @@ class MessagesController < ApplicationController
 	end
 
 	def set_group
-      @group = Group.find(params[:group_id])
+    @group = Group.find(params[:group_id])
   end
 end
 
