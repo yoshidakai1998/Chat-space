9327c7c6c3e5844da58a2957e9149a3713ca7c82
yoshidakai1998 <bkbkai130@gmail.com>
2019-01-27 19:11:00 +0900

    messages_controllerのインデントを修正しました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,12 @@
 class MessagesController < ApplicationController
   before_action :set_group
 
-	def index
+  def index
     @message =Message.new
     @messages = @group.messages.includes(:user)
-	end
+  end
 
-	def create
+  def create
     @message = @group.messages.new(message_params)
     if @message.save
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
@@ -15,17 +15,16 @@ class MessagesController < ApplicationController
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
-	end
+  end
 
-	private
+  private
 
-	def message_params
-	  params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-	end
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
-	def set_group
+  def set_group
     @group = Group.find(params[:group_id])
   end
 end
-
 
