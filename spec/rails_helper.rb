cb5f893d4fc0ecdd50c8ebc94cc902ea166d4342
yoshidakai1998 <bkbkai130@gmail.com>
2019-01-31 20:08:58 +0900

    RSpec test controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,10 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.include FactoryGirl::Syntax::Methods
