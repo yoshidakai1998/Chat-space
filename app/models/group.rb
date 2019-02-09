9af05ff75dfc9dc60df19cf04a69ff0a94ca20db
yoshidakai1998 <bkbkai130@gmail.com>
2019-02-10 02:05:12 +0900

    Add sidebar message

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,13 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true
+
+  def show_final_message
+    if (final_message = messages.last)
+      final_message.content? ? final_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
 
