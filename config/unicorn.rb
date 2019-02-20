76ae7480a138ba3190df5e85218b90631ba623ee
yoshidakai1998 <bkbkai130@gmail.com>
2019-02-20 17:29:06 +0900

    Edit unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,7 @@ worker_processes 1
 
 working_directory "#{app_path}/current"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}shared/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
